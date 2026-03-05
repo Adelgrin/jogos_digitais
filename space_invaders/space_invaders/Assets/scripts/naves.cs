31f0d5e07ac68c234094b1f26821be8f3cb94cd0
Adelgrin <lamaral2002@gmail.com>
2026-03-05 12:04:52 -0300

    movimento aliens

---
+++ space_invaders/space_invaders/Assets/scripts/naves.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class naves : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private float timer = 0.0f;
+    private float waitTime = 1.0f;
+    private float speed = 2.0f;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        var vel = rb2d.velocity;
+        vel.x = speed;
+        rb2d.velocity = vel;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= waitTime)
+        {
+            ChangeState();
+            timer = 0.0f;
+        }
+    }
+    void ChangeState()
+    {
+        var vel = rb2d.velocity;
+        vel.x *= -1;
+        rb2d.velocity = vel;
+    }
+}
